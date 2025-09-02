09462e0c91151e0d9c4a53d2f58f06a29824d143
SuarezGil <isuarez-2021691@kinal.edu.gt>
2025-09-02 15:40:10 -0600

    Se subio el proyecto

---
+++ src/main/java/org/zix/GestionEventosTicketing/GestionEventosTicketingApplication.java
@@ -0,0 +1,13 @@
+package org.zix.GestionEventosTicketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEventosTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEventosTicketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zix/GestionEventosTicketing/GestionEventosTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zix.GestionEventosTicketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEventosTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
