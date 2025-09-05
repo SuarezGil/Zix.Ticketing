80f9d71634f0c0e12477569870d36e3e4e9efaf1
Mcho2021304 <Mcho-2021304@kinal.edu.gt>
2025-09-05 17:14:23 -0600

    en proceso de terminar el menu principal con el login

--- src/main/java/org/zix/GestionEventosTicketing/dominio/service/UsuarioService.java
+++ src/main/java/org/zix/GestionEventosTicketing/dominio/service/UsuarioService.java
@@ -1,4 +1,21 @@
 package org.zix.GestionEventosTicketing.dominio.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zix.GestionEventosTicketing.persistence.crud.UsuarioCrud;
+import org.zix.GestionEventosTicketing.persistence.entity.Usuario;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioCrud usuarioCrud;
+
+    public boolean validarCredenciales(String email, String password) {
+
+        Usuario usuario = usuarioCrud.findByEmail(email);
+
+
+        return usuario != null && usuario.getPassword().equals(password);
+    }
 }
--- src/main/java/org/zix/GestionEventosTicketing/persistence/crud/UsuarioCrud.java
+++ src/main/java/org/zix/GestionEventosTicketing/persistence/crud/UsuarioCrud.java
@@ -1,4 +1,8 @@
 package org.zix.GestionEventosTicketing.persistence.crud;
 
-public class UsuarioCrud {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zix.GestionEventosTicketing.persistence.entity.Usuario;
+
+public interface UsuarioCrud extends JpaRepository<Usuario, Long> {
+    Usuario findByEmail(String email);
 }
